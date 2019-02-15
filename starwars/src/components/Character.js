9f66efd99963310ea4c4f912f07909b91e0b2cf5
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-02-15 12:09:13 -0800

    MVP complete with a few stylings

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,9 +3,27 @@ import React from 'react'
 const Character = props => {
     return (
         <div className='character'>
-            <p>{props.character.name}</p>
+            <p>
+            <strong>Name:</strong>{props.character.name}
+            </p>
+
+            <p>
+                <strong>Height:</strong> {props.character.height}
+            </p>
+            <p>
+                <strong>Weight:</strong> {props.character.mass}
+            </p>
+            <p>
+                <strong>Birth Year:</strong>{props.character.birth_year}
+            </p>
+
         </div>
     )
 }
 
 export default Character;
+
+
+
+
+//Luke
